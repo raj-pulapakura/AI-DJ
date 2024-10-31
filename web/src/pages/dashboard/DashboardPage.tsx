76cd5406b2f8b4bb04d365e40be75e7edd0d48d3
Raj <raj.pulapakura@gmail.com>
2024-10-31 23:04:14 +1100

    Tweaks

--- web/src/pages/dashboard/DashboardPage.tsx
+++ web/src/pages/dashboard/DashboardPage.tsx
@@ -12,48 +12,49 @@ export default function DashboardPage() {
 
     return (
         <div>
-            {
-                isPlaying ?
-                    <motion.div
-                        style={{
-                            width: "100px",
-                            height: "100px",
-                            borderRadius: "100%",
-                            backgroundColor: "white",
-                            border: "2px solid blue",
-                            margin: "0 auto",
-                            marginTop: "200px"
-                        }}
-                        initial={{ scale: 1 }}
-                        animate={{ scale: [1.8, 2, 1.8] }}
-                        transition={{
-                            duration: 1,
-                            repeat: Infinity,
-                            repeatType: "loop",
-                            ease: "easeInOut"
-                        }}
-                    >
-                    </motion.div> : <motion.div
-                        style={{
-                            width: "100px",
-                            height: "100px",
-                            borderRadius: "100%",
-                            backgroundColor: "blue",
-                            border: "none",
-                            margin: "0 auto",
-                            marginTop: "200px"
-                        }}
-                        initial={{ scale: 1 }}
-                        animate={{ scale: [1, 1.2, 1] }}
-                        transition={{
-                            duration: 2,
-                            repeat: Infinity,
-                            repeatType: "loop",
-                            ease: "easeInOut"
-                        }}
-                    >
-                    </motion.div>
-            }
+            <h1 className="text-3xl font-semibold text-center mt-10">AIDJRIEN</h1>
+            <p className="text-center">(pronounced "Adrien")</p>
+            <div className="mx-auto mt-20 w-fit">
+                {
+                    isPlaying ?
+                        <motion.div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                borderRadius: "100%",
+                                backgroundColor: "white",
+                                border: "2px solid blue",
+                            }}
+                            initial={{ scale: 1 }}
+                            animate={{ scale: [1.8, 2, 1.8] }}
+                            transition={{
+                                duration: 1,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                                ease: "easeInOut"
+                            }}
+                        >
+                        </motion.div> : <motion.div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                borderRadius: "100%",
+                                backgroundColor: "blue",
+                                border: "none",
+                            }}
+                            initial={{ scale: 1 }}
+                            animate={{ scale: [1, 1.2, 1] }}
+                            transition={{
+                                duration: 2,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                                ease: "easeInOut"
+                            }}
+                        >
+                        </motion.div>
+                }
+
+            </div>
 
             <AudioCapture />
             <AudioPlayer setIsPlaying={setIsPlaying} />
